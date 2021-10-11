8c9e38bc5f3ec9507e1bdb5df14709e3ca7f9b56
Malcolm Lima <malcolmmellolima@gmail.com>
2021-10-11 12:03:46 -0300

    parte visual e funcionalidades principais feitas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect} from "react";
 import './App.css';
+
 import ChatListItem from "./components/ChatListItem";
+import ChatIntro from "./components/ChatIntro";
+import ChatWindow from "./components/ChatWindow";
 
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from '@material-ui/icons/Chat';
@@ -9,14 +12,25 @@ import SearchIcon from '@material-ui/icons/Search'
 
 
 export default () => {
-  const[chatlist, setChatList] = useState([{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}])
 
+  const[chatlist, setChatList] = useState([
+    {chatId: 1, title: 'Fulano de tal', image:'https://www.w3schools.com/howto/img_avatar2.png'},
+    {chatId: 2, title: 'Fulano de tal', image:'https://www.w3schools.com/howto/img_avatar2.png'},
+    {chatId: 3, title: 'Fulano de tal', image:'https://www.w3schools.com/howto/img_avatar2.png'},
+    {chatId: 4, title: 'Fulano de tal', image:'https://www.w3schools.com/howto/img_avatar2.png'} 
+  ])
+  const [activeChat, setActiveChat] = useState({})
+  const [user, setUser] = useState({
+    id: 1234,
+    avatar: 'https://www.w3schools.com/howto/img_avatar2.png',
+    name:'Marlo Lima'
+  })
 
   return (
     <div className="app-window">
       <div className="sidebar"> 
         <header>
-          <img className="header--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="" />
+          <img className="header--avatar" src={user.avatar} alt="" />
           <div className="header--buttons">
             <div className="header--btn">
               <DonutLargeIcon style={{color: '#919191'}}/>
@@ -38,12 +52,23 @@ export default () => {
         </div>
         <div className="chatlist">
           {chatlist.map((item, key)=>
-            <ChatListItem key={key}/>
+            <ChatListItem 
+              key={key} 
+              data={item}
+              active={activeChat.chatId === chatlist[key].chatId}
+              onClick={()=>setActiveChat(chatlist[key])}/>
           )}   
         </div>
       </div>
       <div className="contentarea">
-        ...
+        {activeChat.chatId !== undefined &&
+          <ChatWindow
+            user={user}
+          />
+        }
+        {activeChat.chatId === undefined &&
+          <ChatIntro />
+        }
       </div>
     </div>
   )
---
+++ src/components/ChatIntro.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './ChatIntro.css'
+
+export default () => {
+    return (
+        <div className="chatIntro">
+            <img src="https://www.inclutopia.com.br/_files/200000598-0dbb10dbb4/450/desenho%20whatsapp.png?ph=d54eb29570" alt=""/>
+            <h1>Mantenha seu celular conectado</h1>
+            <h2>O WhatsApp conecta ao seu celular para sincronizar suas mensagens. <br/> Para reduzir o uso de dados, conecte seu celular a uma rede Wi-Fi.</h2>
+        </div>
+    )
+}
